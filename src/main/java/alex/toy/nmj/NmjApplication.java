43ef18e83bba0b46fe3d81f0530742a1cfeb0280
giibeom <kpmyung@gmail.com>
2023-01-10 21:05:18 +0900

    chore: 스프링 부트 프로젝트를 생성한다

---
+++ src/main/java/alex/toy/nmj/NmjApplication.java
@@ -0,0 +1,12 @@
+package alex.toy.nmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmjApplication.class, args);
+	}
+}
