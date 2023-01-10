d01257432e0b2715411b52d5e69bef1501ec8102
명기범 (Alex) <kpmyung@gmail.com>
2023-01-10 21:21:17 +0900

    (#2) 백엔드 프로젝트를 생성한다
    * chore: 스프링 부트 프로젝트를 생성한다
    
    * chore: 데이터베이스 설정 파일을 세팅한다

---
+++ src/test/java/alex/toy/nmj/NmjApplicationTests.java
@@ -0,0 +1,12 @@
+package alex.toy.nmj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
