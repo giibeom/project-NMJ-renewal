98157e7f4c93a52daf9194b5bdb9ce126bcf3ef6
giibeom <kpmyung@gmail.com>
2023-01-18 02:59:19 +0900

    feat(#7): 회원 타입과 회원 상태를 분리한다

---
+++ src/test/java/alex/toy/nmj/fixture/MemberFixture.java
@@ -0,0 +1,57 @@
+package alex.toy.nmj.fixture;
+
+import alex.toy.nmj.member.domain.Member;
+import alex.toy.nmj.member.domain.MemberType;
+
+public enum MemberFixture {
+    일반_회원_giibeom("dev.gibeom@gmail.com", "비밀번호486",
+            "giibeom", "010-7777-7777", MemberType.USER),
+    매장_회원_Alex("kpmyung@gmail.com", "비밀번호486",
+            "Alex", "010-1234-5678", MemberType.STORE),
+    관리자_회원_기범("admin@gmail.com", "그대여아무걱정하지말아요",
+            "기범관리자", "010-0707-0707", MemberType.ADMIN);
+
+    private final String email;
+    private final String password;
+    private final String name;
+    private final String phone;
+    private final MemberType type;
+
+    MemberFixture(String email, String password, String name, String phone, MemberType type) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.type = type;
+    }
+
+    public Member 엔티티_생성() {
+        return Member.builder()
+                .email(this.email)
+                .password(this.password) // TODO : 암호화 진행 (시큐리티)
+                .name(this.name)
+                .phone(this.phone)
+                .type(this.type)
+                .build();
+    }
+
+    public String 이메일() {
+        return email;
+    }
+
+    public String 비밀번호() {
+        return password;
+    }
+
+    public String 이름() {
+        return name;
+    }
+
+    public String 전화번호() {
+        return phone;
+    }
+
+    public MemberType 회원타입() {
+        return type;
+    }
+}
